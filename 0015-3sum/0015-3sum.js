1f98511b9991e68d29afdbb3ccb588c48cfa19c7
Midori Li <108377805+midorili@users.noreply.github.com>
2023-07-29 22:11:07 -0700

    Time: 143 ms (99.85%), Space: 59.8 MB (27.55%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,44 +3,36 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a-b)
-    var res = [];
-    for (var i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] == nums[i-1]) {
+    var res = []
+  nums.sort((a,b) => a - b)
+    for (var i = 0; i < nums.length; i ++) {
+        if (i > 0 && nums[i] === nums[i-1]) {
             continue;
         }
-        // for (var j = i+1; j < nums.length; j++) {
-        //      if (j > i+1 && nums[j] == nums[j-1]) {
-        //     continue;
-        // }
-        //     for (var k = j+1; k < nums.length; k++) {
-        //      if (k > j+1 && nums[k] == nums[k-1]) {
-        //     continue;
-        // }
-        //         if (nums[i] + nums[j] + nums[k] === 0) {
-        //             res.push([nums[i], nums[j], nums[k]])
-        //         }
-        //     }
-        // }
         var target = 0 - nums[i];
         var left = i + 1;
-        var right = nums.length - 1;
+        var right = nums.length - 1
         while (right > left) {
-            var sum = nums[left] + nums[right];
+            var sum = nums[left] + nums[right]
+
             if (sum > target) {
-                right --;
+                right --
             } else if (sum < target) {
                 left ++
-            } else{
+            } else {
                 res.push([nums[i], nums[left], nums[right]])
-                while(nums[left] === nums[left+1]) left++
-                while(nums[right] === nums[right+1]) right--
-                left++;
-                right--;
+            while (nums[left] === nums[left + 1]) {
+            left ++
+        }
+        while (nums[right] === nums[right + 1]) {
+            right --
+        }
+                  left++;
+            right--;
 
             }
         }
-    }
     
-    return res; 
+    }
+    return res;
 };
