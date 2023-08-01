89774896ba44af0f35dce1463bbb4925c0fa5f50
Midori Li <108377805+midorili@users.noreply.github.com>
2023-07-31 23:36:32 -0700

    Time: 87 ms (44.14%), Space: 51.2 MB (90.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,16 +4,16 @@
  */
 var maxProfit = function(prices) {
     if (prices.length < 2) {
-        return 0;
+        return 0
     }
-
-    var buy = prices[0];
-    var sell = 0;
-
-    for (var i = 1; i < prices.length; i++) {
-        buy = Math.min(buy, prices[i]);
-        sell = Math.max(sell, prices[i] - buy);
+    
+    let buy = prices[0];
+    let sell = 0;
+    
+    for (var i = 0; i < prices.length; i++) {
+       buy = Math.min(buy, prices[i])
+       sell = Math.max(sell, prices[i]-buy)
     }
-
-    return sell;
-};
+    
+    return sell; 
+}
