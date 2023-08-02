8e2c2b9daaf90c711134f6482c390ae65289d936
Midori Li <108377805+midorili@users.noreply.github.com>
2023-08-01 21:45:31 -0700

    Time: 66 ms (84.64%), Space: 44.9 MB (72.33%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head || !head.next) {
+        return false;
+    }
+    // var slow = this.head.next
+    // var fast = this.head.next.next
+    var slow = head;
+    var fast = head.next;
+    while(slow !== fast) {
+        if (!fast || !fast.next) {
+        return false; 
+    }
+        slow = slow.next;
+        fast = fast.next.next; 
+    }
+return true; 
+ 
+};
