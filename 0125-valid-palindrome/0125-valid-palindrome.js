50a6451eeea787d9780bef7ef41431814e41caa2
Midori Li <108377805+midorili@users.noreply.github.com>
2023-08-25 14:32:05 -0700

    Time: 87 ms (13.30%), Space: 48.5 MB (24.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,7 +4,10 @@
  */
 var isPalindrome = function(s) {
   var lowercase = s.toLowerCase().split(' ').join('').replace(/[^0-9a-z]/gi, '');
-    var reversed = lowercase.split('').reverse().join('').replace(/[^0-9a-z]/gi, '')
+    console.log(lowercase)
+    var reversed = lowercase.split('').reverse().join('')
+    console.log(reversed)
+    // var reversed = lowercase.split('').reverse().join('').replace(/[^0-9a-z]/gi, '')
     if (lowercase === reversed) {
         return true
     }
