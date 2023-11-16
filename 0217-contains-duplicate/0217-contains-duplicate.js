83389ce0497a4a93c274d7ac32bf631d84c81f64
Midori Li <108377805+midorili@users.noreply.github.com>
2023-11-15 17:40:15 -0800

    Time: 65 ms (96.09%), Space: 53.7 MB (66.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -6,9 +6,9 @@ var containsDuplicate = function(nums) {
     var seen = {};
     for (var i = 0; i < nums.length; i++) {
         if (seen[nums[i]] === undefined) {
-            seen[nums[i]] = 'saw';
-        } else if (seen[nums[i]]) {
-            return true; 
+            seen[nums[i]] = nums[i]
+        } else {
+            return true;
         }
     }
     return false; 
